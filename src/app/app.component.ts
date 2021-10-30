39fb173ee775fe7d4d85d5276dc965419cf37088
admin <admin@admin.com>
2021-10-31 00:06:57 +0600

    Level 4. Created control buttons and output information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,44 @@ import {Card, CardDeck} from "../lib/CardDeck";
 export class AppComponent {
   cardDeck: CardDeck;
   cards: Card[];
+  score = 0;
 
   constructor() {
     this.cardDeck = new CardDeck();
     this.cards = this.cardDeck.getCards(2);
+    this.score = this.calcScore(this.cards);
+  }
+
+  getCard() {
+    if (this.isUserWin() || this.isUserLose()) {
+      return;
+    }
+
+    const card = this.cardDeck.getCard();
+    this.cards.push(card);
+    this.score += this.calcScore([card]);
+  }
+
+  resetGame() {
+    this.cardDeck = new CardDeck();
+    this.cards = this.cardDeck.getCards(2);
+    this.score = this.calcScore(this.cards);
+  }
+
+  calcScore(cards: Card[]) {
+    let result = 0;
+    cards.forEach(card => {
+      result += card.getScore();
+    });
+
+    return result;
+  }
+
+  isUserWin() {
+    return this.score === 21;
+  }
+
+  isUserLose() {
+    return this.score > 21;
   }
 }
