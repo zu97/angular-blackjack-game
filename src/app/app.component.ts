f9e783e3d707528168989d209a5d9f93d819fde5
admin <admin@admin.com>
2021-10-30 23:21:14 +0600

    Level 3. Updated app class. Made card output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Card, CardDeck} from "../lib/CardDeck";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'homework52';
+  cardDeck: CardDeck;
+  cards: Card[];
+
+  constructor() {
+    this.cardDeck = new CardDeck();
+    this.cards = this.cardDeck.getCards(2);
+  }
 }
